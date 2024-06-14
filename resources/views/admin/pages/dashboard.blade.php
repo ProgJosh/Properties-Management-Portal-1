61c818dae4dd02d8bd94bd6cd2f5123e422b531f
creativesaiful <saifulislamsapon@gmail.com>
2024-06-14 10:21:46 +0600

    first commit

---
+++ resources/views/admin/pages/dashboard.blade.php
@@ -0,0 +1,36 @@
+@extends('admin.layouts.admin')
+
+@section('content')
+<div class="row">
+    <div class="col-md-6 col-xl-12">
+        <div class="card-box tilebox-one">
+            
+            <h5 class="text-muted text-uppercase mb-3 mt-0">Welcome To  Dashboard</h5>
+            
+        </div>
+    </div>
+
+    
+</div>
+
+@endsection
+
+
+
+
+
+{{-- <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
+            </div>
+        </div>
+    </div>
+</x-app-layout> --}}
